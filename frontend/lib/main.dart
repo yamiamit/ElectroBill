0902c6735e182cad3d20f0931e6eb146c7a09e6c
yamiamit <way2aamit@gmail.com>
2025-06-28 18:29:43 +0530

    updated README.md

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:frontend/screens/welcome_screen.dart';
@@ -19,6 +20,27 @@ class Myapp extends StatefulWidget {
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class _MyappState extends State<Myapp> {
+=======
+import 'package:frontend/screens/home_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:frontend/screens/welcome_screen.dart';
+
+void main() {
+  //make sure to pass here isloggedin bool
+  runApp(const Myapp());
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+class _MyappState extends State<Myapp> {
+
+>>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
@@ -32,8 +54,16 @@ class _MyappState extends State<Myapp> {
       debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
       home: WelcomeScreen(),
+<<<<<<< Updated upstream
       // home: widget.isLoggedIn ? HomeScreen() : loginScreen(),
       builder: EasyLoading.init(),
     );
   }
 }
+=======
+      // home: widget.isLoggedIn ? FaultFinderApp() : loginScreen(),
+    );
+  }
+}
+
+>>>>>>> Stashed changes
--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,17 @@ import Foundation
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
+<<<<<<< Updated upstream
+=======
+import shared_preferences_foundation
+>>>>>>> Stashed changes
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+<<<<<<< Updated upstream
+=======
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+>>>>>>> Stashed changes
 }
